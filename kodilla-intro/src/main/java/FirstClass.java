2beee50dea9237cce58e9f8df01f3f87d328d9b2
Andrzej Eckert <andrzej.e007@gmail.com>
2023-04-19 20:06:02 +0200

    INIT VERION

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        String welcome = "Hello from FirstClass!";
+        System.out.println(welcome); // [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World"); // [3]
+        System.out.println("Andrzej"); // [3]
+        System.out.println(52); // [3]
+        System.out.println('E'); // [3]
+    }
+}
