156f2a9c39b737805eabf6812d0220a5b41880b9
Andrzej Eckert <andrzej.e007@gmail.com>
2023-05-22 19:29:30 +0200

    3v2

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -2,6 +2,8 @@ package com.kodilla.basic_assertion;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTestSuite {
     @Test
     public void testSum(){
@@ -9,6 +11,30 @@ public class CalculatorTestSuite {
         int a= 5;
         int b = 8;
         int sumResult = calculator.sum(a, b);
-        ResultChecker.assertEquals(13, sumResult);
+        assertEquals(13, sumResult);
+    }
+    @Test
+    public void testSubstract(){
+        Calculator calculator = new Calculator();
+        int a= 5;
+        int b = 8;
+        double substractResult = calculator.substract(a, b);
+        assertEquals(-03, substractResult, 0.1);
+    }
+    @Test
+    public void testSquare(){
+        Calculator calculator = new Calculator();
+        int a= 5;
+        double squareResult = calculator.square(a);
+        assertEquals(25, squareResult, 0.1);
+
+        a= -5;
+        squareResult = calculator.square(a);
+        assertEquals(25, squareResult, 0.1);
+
+        a= -0;
+        squareResult = calculator.square(a);
+        assertEquals(0, squareResult, 0.1);
+
     }
 }
---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/PrimeChecker.java
@@ -0,0 +1,86 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+public class PrimeChecker {
+    private int count;
+    private PrimeChecker checker = new PrimeChecker();
+    @Test
+    public boolean isPrime(int a){
+        if (a < 2){
+            return false;
+        }
+        for (int i = 2; i < a; i++){
+            if (a % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int getCount(){
+        return count;
+    }
+
+    public void incrementCount(){
+        this.count++;
+    }
+
+    //private PrimeChecker checker = new PrimeChecker();
+    @Test
+    public void ShouldReturnFalseWhenPassingNotPrimeNumber(){
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(14);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertFalse(result);
+    }
+    @Test
+    public void shouldReturnTrueWhenPassingPrimeNumber() {
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(13);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenPassingTwo() {
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(2);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenPassingOne() {
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(1);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenPassingZero() {
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(0);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenPassingNegativeNumber() {
+        //PrimeChecker checker = new PrimeChecker();
+        boolean result = checker.isPrime(-6);
+        checker.incrementCount();
+        System.out.println("Count: " + checker.getCount());
+        assertFalse(result);
+    }
+}
