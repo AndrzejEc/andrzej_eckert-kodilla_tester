158110c8cba1f4f800593e5fed7786e040426388
Andrzej Eckert <andrzej.e007@gmail.com>
2023-06-09 11:46:33 +0200

    [4.4]

---
+++ kodilla-colections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -0,0 +1,30 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.Objects;
+
+public class Stamp {
+    private String name;
+    private Double width;
+    private Double height;
+    private Boolean stamped;
+
+    public Stamp(String name, Double width, Double height, Boolean stamped) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.stamped = stamped;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stamp stamp = (Stamp) o;
+        return Objects.equals(name, stamp.name) && Objects.equals(width, stamp.width) && Objects.equals(height, stamp.height) && Objects.equals(stamped, stamp.stamped);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, width, height, stamped);
+    }
+}
---
+++ kodilla-colections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -0,0 +1,19 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("clouds", 2.0,2.0, true));
+        stamps.add(new Stamp("clouds", 2.0,3.0, true));
+        stamps.add(new Stamp("forest", 2.0,2.0, false));
+        stamps.add(new Stamp("lake", 2.0,2.0, true));
+        stamps.add(new Stamp("grass", 2.0,2.0, true));
+        stamps.add(new Stamp("stones", 2.0,2.0, true));
+        stamps.add(new Stamp("clouds", 2.0,2.0, true));
+
+        System.out.println(stamps.size());
+    }
+}
