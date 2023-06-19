493cb0f1af55b52bf1d45546dc3db3db71b314c5
Andrzej Eckert <andrzej.e007@gmail.com>
2023-06-19 16:36:10 +0200

    [6.1]

--- kodilla-stream/src/test/java/com/kodilla/stream/UserManagerTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/UserManagerTestSuite.java
@@ -1,4 +1,35 @@
+package com.kodilla.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import static com.kodilla.stream.UserManager.filterChemistGroupUsernames;
+//import static com.kodilla.stream.UserManager    ;
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserManagerTestSuite {
-  
+    @Test
+    void getWorksusername(){
+        List<String> chemistsList = new ArrayList<>();
+        chemistsList.add("Walter White");
+        chemistsList.add("Gale Boettiche");
+        List<String> result = UsersManager.filterChemistGroupUsernames();
+        assertEquals(chemistsList, result);
+    }
+
+    @Test
+    void getCorrectAgeFilter(){
+        List<String> chemistsList = new ArrayList<>();
+        List<String> result = UsersManager.filterAgeUsernames(30);
+        assertEquals(5, result.size());
+    }
+    @Test
+    void getCorrectAgeGroupFilter(){
+        //List<String> chemistsList = new ArrayList<>();
+        List<String> result = UsersManager.filterAgeGroupUsernames("Chemists", 45);
+        assertEquals(1, result.size());
+    }
+
 }
