acc65f7a6003a00257f0ca04a6e677a9b8722161
Andrzej Eckert <andrzej.e007@gmail.com>
2023-06-19 17:03:54 +0200

    [3.5]

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,11 +1,10 @@
 package com.kodilla.basic_assertion;
 
 public class Calculator {
-    int a;
-    int b;
+
     public int sum(int a, int b){
         return a + b;
-    };
+    }
     public  int substract( int a, int b){
         return a - b;
     }
