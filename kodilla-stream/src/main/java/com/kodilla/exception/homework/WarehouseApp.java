f3423064eff04bbb47f10d9471f1114df5581e58
Andrzej Eckert <andrzej.e007@gmail.com>
2023-07-04 20:09:10 +0200

    [6.4]

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -7,9 +7,18 @@ public class WarehouseApp {
         warehouse.addOrder(new Order("first order"));
         warehouse.addOrder(new Order("second order"));
         warehouse.addOrder(new Order("third order"));
-
+        //order exist
         try {
-            result = warehouse.getOrder("thir order").getNumber();
+            result = warehouse.getOrder("third order").getNumber();
+            System.out.println("Found: " + result);
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Order not found");
+        } finally {
+            System.out.println("Thanks for using Our Warehouse");
+        }
+        //order no exist
+        try {
+            result = warehouse.getOrder("fourth order").getNumber();
             System.out.println("Found: " + result);
         } catch (OrderDoesntExistException e) {
             System.out.println("Order not found");
